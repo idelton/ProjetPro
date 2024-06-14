0ae47fc763b80b5ea2c1ee3722567fcdf755e716
idelton <94796876+idelton@users.noreply.github.com>
2024-06-14 06:19:05 -0700

    Create Js_function_flechées.js

---
+++ Js_function_flechées.js
@@ -0,0 +1,14 @@
+// 1
+/*Écrire cette fonction en fonction flechées
+function add(a, b) {
+  return a + b;
+} */
+const add = (a,b) => a+ b;
+console.log(add(3,3))
+
+//exo2
+/*function greet(name) {
+  return 'Hello, ' + name + '!';
+} */
+const greet= (name) => "hello" + name + "!" ;
+console.log(greet(' paul'));
