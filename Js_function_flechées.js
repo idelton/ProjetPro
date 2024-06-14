62028189b748cbbc83a703782a7bb973ea7d26e0
idelton <94796876+idelton@users.noreply.github.com>
2024-06-14 07:51:09 -0700

    Update Js_function_flechées.js

--- Js_function_flechées.js
+++ Js_function_flechées.js
@@ -12,3 +12,18 @@ console.log(add(3,3))
 } */
 const greet= (name) => "hello" + name + "!" ;
 console.log(greet(' paul'));
+
+/*exercice 3
+1) Écrivez une fonction fléchée qui prend un tableau
+  de noms et retourne un tableau de salutations pour
+  chaque nom. Par exemple, ['Alice', 'Bob'] devrait
+  retourner ['Hello, Alice!', 'Hello, Bob!'].*/
+
+const noms = ['idelton','arthur'];
+const bonjour = noms.map((nomss) => `hello ${nomss}`) 
+console.log(bonjour)
+
+
+
+
+
